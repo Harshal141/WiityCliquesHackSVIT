74d6f6f966277ea6c98ecd93b22eb54786adfe7d
SahilKamate <sahilkamate03@gmail.com>
2022-05-01 01:29:58 +0530

    added center map

--- map/map_app.js
+++ map/map_app.js
@@ -1,5 +1,5 @@
 mapboxgl.accessToken =
-    "pk.eyJ1Ijoic2FoaWwta2FtYXRlIiwiYSI6ImNsMmttNzR1ODA1M3ozaW5vemw4OWFubnkifQ.Q_1fGDPAy94gxKirc0l8Og"
+    "pk.eyJ1Ijoic2FoaWwta2FtYXRlIiwiYSI6ImNsMm01dzk4ODBldzUzanJ1dm9pN3pzcDQifQ.32S_1bF86VEmWYaCHjlswQ"
 
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
     enableHighAccuracy: true
@@ -18,7 +18,7 @@ function setupMap(center) {
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
         center: center,
-        zoom: 15
+        zoom: 16
     })
 
     const nav = new mapboxgl.NavigationControl()
@@ -28,5 +28,5 @@ function setupMap(center) {
         accessToken: mapboxgl.accessToken
     })
 
-    map.addControl(directions, "top-left")
+    // map.addControl(directions, "top-left")
 }
